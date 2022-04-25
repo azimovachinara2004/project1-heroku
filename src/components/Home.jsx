fd79c3c7499225ba6994265b48829f4c65689445
Chinara <azimovachinara2004@gmail.com>
2022-04-25 21:05:49 +0300

    added"Class component"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import {useDispatch,useSelector} from 'react-redux'
 import {callToAPI} from './homeSlice'
 import Cards from './Cards'
 import Pagination from './Pagination'
+
 //import { useParams } from 'react-router-dom'
 function Home(){
     const [selectCategory,setSelectCategory]=useState("")
@@ -26,7 +27,8 @@ function Home(){
         let list=flowerList.filter(flower=>selectCategory ===flower.category || selectCategory===""|| selectCategory==="All")
         setFilteredFlowerList(list)
       },[selectCategory])
-     let catList=["All",...new Set(flowerList.map(item =>item.category))]
+     
+      let catList=["All",...new Set(flowerList.map(item =>item.category))]
      useEffect(()=>{
       setHowManyPages(Math.ceil(filteredFlowerList.length /howManyElementsPerPage))
   },[filteredFlowerList])
@@ -35,7 +37,7 @@ function Home(){
   const flowersToShow=filteredFlowerList.slice(start,end)
         return(
                  <>
-                 
+
                  {
         catList.map(category=><button className='category'
         key={category}
